f76007861a35bac61a42e97c171464d6f23b14cd
mew1876 <mew1876@gmail.com>
2018-04-26 11:11:04 -0500

    Created simple Point class and added point cloud parsing

---
+++ CS513-FinalProject/DTM_Generator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS513_FinalProject
+{
+    class DTM_Generator
+    {
+        static List<Point> pointCloud = new List<Point>();
+
+        static void Main(string[] args)
+        {
+            loadPointCloud("../final_project_point_cloud.fuse");
+            foreach(Point point in pointCloud)
+            {
+                Console.WriteLine(point);
+            }
+            Console.ReadLine(); //To keep console open till keypress
+        }
+
+        static void loadPointCloud(string path)
+        {
+            foreach(string line in File.ReadLines(path))
+            {
+                string[] elements = line.Split(' ');
+                Point point = new Point(float.Parse(elements[0]), float.Parse(elements[1]), float.Parse(elements[2]), int.Parse(elements[3]));
+                pointCloud.Add(point);
+            }
+        }
+    }
+}
---
+++ CS513-FinalProject/Point.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS513_FinalProject
+{
+    class Point
+    {
+        public float latitude;
+        public float longitude;
+        public float elevation;
+        public int intensity;
+
+        public Point(float latitude, float longitude, float elevation, int intensity)
+        {
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.elevation = elevation;
+            this.intensity = intensity;
+        }
+
+        public override string ToString()
+        {
+            return "Latitude: " + latitude + "\tLongitude: " + longitude + "\tElevation: " + elevation + "\tIntensity: " + intensity;
+        }
+    }
+}
